6a6ab56fc04cf1685dafa5f61a485572caf1f3df
Damjan Miloshevski <d.miloshevski@gmail.com>
2016-03-04 22:37:23 +0100

    imported

---
+++ Javelin/src/main/java/com/javelin/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.javelin.repository;
+
+import com.javelin.model.Comment;
+import com.javelin.model.CommentId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, CommentId> {
+}
